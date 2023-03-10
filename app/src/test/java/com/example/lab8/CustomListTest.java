6cfa6a06e46baabcd606fcc03808bc2cbdbc4f90
“Prateek <prateekj@ualberta.ca>
2023-03-09 18:47:56 -0700

    RED: test add city

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -12,5 +12,18 @@ import java.util.ArrayList;
 
 public class CustomListTest {
 
+    private ArrayList<City> citylist = new ArrayList<>();
+
+    @Test
+    public void testAddCity(){
+        CustomList list = new CustomList(null,citylist);
+        City city = new City("Edmonton","AB");
+
+        list.addCity(city);
+        assertEquals(1,list.getCount());
+
+    }
+
+
 
 }
